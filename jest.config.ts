c0844f355f2dd2c72947a39644920b0d3fc579a5
Antonio Esposito <antonio.esposito4@live.it>
2024-08-31 16:26:05 +0200

    rxjs and jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,8 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)']
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
 };
 
 export default config;
